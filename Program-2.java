f75c01943047045611e15d598dce5c8ba2928a4a
Deepa Ajjannavar <66908622+Deepcode14@users.noreply.github.com>
2023-07-17 22:50:46 +0530

    Add files via upload

---
+++ Program-2.java
@@ -0,0 +1,35 @@
+package Program;
+
+import java.util.Scanner;
+
+public class Program-2 {
+	public static void main(String args[])
+	{ 
+		int num,line=0;
+	Scanner in=new Scanner(System.in);
+	num=in.nextInt();
+	if(num==1)
+	{
+		System.out.println(num);
+	}
+	else {
+	for ( int i = 1; i <= num; i++)
+ {
+	if(num%2==0)
+	{
+		line=2*(i-1)-1;
+		if(line==-1)
+		{
+			continue;
+		}
+		System.out.println(line+" ");
+	}
+	else
+	{
+		line=2*i-1;
+		System.out.println(line+" ");
+	}
+ }
+ }
+	}
+}
